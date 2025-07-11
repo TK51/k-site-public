823b6d2cc22552a6f40f86066df1f3e3f76bc6b5
Taras Khamardiuk <79279917+TK51@users.noreply.github.com>
2025-07-11 03:20:27 +0300

    Update utils.py

--- content/scripts/utils.py
+++ content/scripts/utils.py
@@ -1,1 +1,5 @@
+whatever you place here -> will be updated by GitHub in almost no time
+so, you could easily run you portfolio, public project, blog, or whatever you need
+using this simple site generator
 
+- Kay
